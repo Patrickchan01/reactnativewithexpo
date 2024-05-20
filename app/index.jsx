13c240cce94ba2bfc4542c68c89183db1ba37981
Patrick Chan <patrickchan@PatrickdeMBP.lan>
2024-05-20 18:03:32 +0800

    complete the UI of sign-in&sign-up page

--- app/index.jsx
+++ app/index.jsx
@@ -1,26 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, ScrollView, Text, View } from 'react-native'
-import { Redirect,router } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants';
 import CustomButton from '../components/CustomButton';
 
 export default function Index() {
   return (
-  <SafeAreaView className="bg-primary h-full">
-    <ScrollView
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView
         contentContainerStyle={{
           height: "100%",
         }}
       >
-    <View className="w-full flex justify-center items-center h-full px-4">
+        <View className="w-full flex justify-center items-center h-full px-4">
 
-      <Image
+          <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
             resizeMode="contain"
           />
-      <Image
+          <Image
             source={images.cards}
             className="max-w-[380px] w-full h-[298px]"
             resizeMode="contain"
@@ -43,15 +43,15 @@ export default function Index() {
             Where Creativity Meets Innovation: Embark on a Journey of Limitless
             Exploration with Aora
           </Text>
-          <CustomButton 
-          title='Continue with email'
-          handlePress = {() => router.push('./sign-in')}
-          containerStyles="w-full mt-7"/>
-      </View>
-      <StatusBar backgroundColor='#161622' style='light'/>
-    </ScrollView>
+          <CustomButton
+            title='Continue with email'
+            handlePress={() => router.push('./sign-in')}
+            containerStyles="w-full mt-7" />
+        </View>
+        <StatusBar backgroundColor='#161622' style='light' />
+      </ScrollView>
 
-  </SafeAreaView>
+    </SafeAreaView>
   );
 }
 
