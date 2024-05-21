34d43b8ca22bac35e0b24ddc62bab977ede2d562
Patrick Chan <patrickchan@PatrickdeMBP.lan>
2024-05-21 13:59:46 +0800

    fetch data

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,28 +1,34 @@
-import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
-import React,{useState} from 'react'
+import { View, Text, FlatList, Image, RefreshControl, Alert } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import SearchInput from '../../components/SearchInput'
 import Trending from '../../components/Trending'
 import EmptyState from '../../components/EmptyState'
+import { getAllPosts } from '../../lib/appwrite'
+import useAppWrite from '../../lib/useAppWrite'
+import VideoCard from '../../components/VideoCard'
 
 
 const Home = () => {
-const [refreshing, setRefreshing] = useState(false)
-const onRresh = async () => {
-  setRefreshing(true);
-  // re call videos 
-  setRefreshing(false);
-}
 
+  const { data: posts, refetch } = useAppWrite(getAllPosts);
+  const [refreshing, setRefreshing] = useState(false)
+  const onRresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  }
   return (
     <SafeAreaView className='bg-primary h-full'>
       <FlatList
-        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        data={posts}
         // data={[]}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
-          <Text className='text-3xl text-white'>{item.id}</Text>
+          <VideoCard 
+          video={item}
+          />
         )}
         ListHeaderComponent={() => (
           <View className='my-6 px-4 space-y-6'>
@@ -36,10 +42,10 @@ const onRresh = async () => {
                 </Text>
               </View>
               <View className='mt-1.5'>
-                <Image 
-                source={images.logoSmall}
-                className='w-9 h-10'
-                resizeMode='contain'
+                <Image
+                  source={images.logoSmall}
+                  className='w-9 h-10'
+                  resizeMode='contain'
                 />
               </View>
             </View>
@@ -48,21 +54,21 @@ const onRresh = async () => {
               <Text className='text-gray-100 text-lg font-pregular mb-3'>
                 Latest Videos
               </Text>
-            <Trending 
-            posts={[{id:1},{id:2},{id:3}] ?? []}
-            />
+              <Trending
+                posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []}
+              />
             </View>
           </View>
         )}
         ListEmptyComponent={() => (
-          <EmptyState 
-          title='No Videos Found'
-          subtitle='Be the first one to upload the video'
+          <EmptyState
+            title='No Videos Found'
+            subtitle='Be the first one to upload the video'
           />
         )}
-        refreshControl={<RefreshControl 
-        refreshing={refreshing}
-        onRefresh={onRresh}
+        refreshControl={<RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRresh}
         />}
       />
     </SafeAreaView>
